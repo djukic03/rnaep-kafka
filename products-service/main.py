4b83d9319501dcb83d9b052d881cc11612abd0c6
djukic03 <djukic.nemanja003@gmail.com>
2026-03-04 03:32:59 +0100

    small fix

--- products-service/main.py
+++ products-service/main.py
@@ -4,12 +4,12 @@ from models import Product
 
 app = FastAPI(title="Products Service")
 
-products_db: List[Product] = [
-    Product(id=1, name="Laptop", price=1500.0, quantity=10),
-    Product(id=2, name="Mouse", price=25.0, quantity=50)
-]
+products_db = {
+    1: Product(id=1, name="Laptop", price=1500.0, quantity=10),
+    2: Product(id=2, name="Mouse", price=25.0, quantity=50)
+}
 
-@app.get("/products", response_model=List[Product])
+@app.get("/products")
 def get_products():
     return products_db
 
